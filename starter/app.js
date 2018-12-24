371109dbdeebc1cf62c7ff17b81c9550544045db
damianjnc <damianjnc@gmail.com>
2018-12-24 06:12:13 +0000

    ui improvements

--- starter/app.js
+++ starter/app.js
@@ -263,6 +263,20 @@ var UIController = (function () {
           year = now.getFullYear();
           document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
         },
+
+        changedType: function(){
+          var fields = document.querySelectorAll(
+              DOMstrings.inputType + ',' +
+              DOMstrings.inputDescription  +
+              ',' + DOMstrings.inputValue
+          );
+          nodeListForEach(fields, function (cur) {
+              cur.classList.toggle('red-focus');
+              document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+
+          });
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -288,6 +302,7 @@ var controller = (function (budgetCtrl, UICtrl) {
                 }
             });
             document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+            document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
 
         };
